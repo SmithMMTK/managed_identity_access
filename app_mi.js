743aca613922012b383b5dded36efddd91d72520
SmithMMTK <smithm@hotmail.co.th>
2025-03-24 16:22:04 +0700

    1622

---
+++ app_mi.js
@@ -0,0 +1,37 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const accountName = process.env.STORAGE_ACCOUNT_NAME;
+const containerName = process.env.BLOB_CONTAINER_NAME;
+const blobName = process.env.BLOB_NAME;
+
+async function main() {
+  const credential = new DefaultAzureCredential(); // Will pick up the VM's managed identity
+
+  const blobServiceClient = new BlobServiceClient(
+    `https://${accountName}.blob.core.windows.net`,
+    credential
+  );
+
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blobClient = containerClient.getBlobClient(blobName);
+
+  const downloadBlockBlobResponse = await blobClient.download();
+  const downloaded = await streamToString(downloadBlockBlobResponse.readableStreamBody);
+
+  console.log("Blob content:", downloaded);
+}
+
+main().catch((err) => {
+  console.error("Error:", err.message);
+});
+
+function streamToString(readableStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    readableStream.on("data", (data) => chunks.push(data.toString()));
+    readableStream.on("end", () => resolve(chunks.join("")));
+    readableStream.on("error", reject);
+  });
+}
+
